1d2c97c2dd35a0827d9c98cc8dd30b8f22518008
jamaljeong <jamaljeong@tecace.com>
2026-08-19 10:22:51 +0900

    feat: add ~/.local/bin to PATH

--- zsh/conf.d/10-path.zsh
+++ zsh/conf.d/10-path.zsh
@@ -1,5 +1,8 @@
 # PATH additions
 
+# local bin (claude, etc.)
+[[ -d "$HOME/.local/bin" ]] && [[ ":$PATH:" != *":$HOME/.local/bin:"* ]] && export PATH="$HOME/.local/bin:$PATH"
+
 # pnpm
 if [[ -d "$HOME/Library/pnpm" ]]; then
 	export PNPM_HOME="$HOME/Library/pnpm"
